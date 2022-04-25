b8a50e12bb7a3f2d4ba5bc58a1a08ae34fdd2db2
MartinKotac <you@example.com>
2022-04-25 23:42:47 +0200

    final commit

---
+++ src/main/java/com/example/lab02emt/web/restApi/AuthorRestController.java
@@ -0,0 +1,34 @@
+package com.example.lab02emt.web.restApi;
+
+import com.example.lab02emt.model.Author;
+import com.example.lab02emt.model.Book;
+import com.example.lab02emt.service.AuthorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/authors")
+public class AuthorRestController {
+
+    private final AuthorService authorService;
+
+    public AuthorRestController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping
+    public List<Author> findAll() {
+        return authorService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> findById(@PathVariable Long id) {
+        return authorService.findById(id)
+                .map(author -> ResponseEntity.ok().body(author))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+}
